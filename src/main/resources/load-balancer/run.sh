44c11d8a76b5c38d3af04b0a44c375a0d5258276
kushan <chinthakaak@gmail.com>
2018-01-02 09:45:15 +0530

    addd

--- src/main/resources/load-balancer/run.sh
+++ src/main/resources/load-balancer/run.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-docker rmi kraydel:v1.0.0-rc1
+# Clean up container / volume / image
+docker stop nginx-lbl
+docker rm nginx-lbl
 docker volume rm conf-lbl
+docker rmi kraydel:v1.0.0-rc1
+
+# Volume mounts
 docker volume create conf-lbl
+
+# Docker image creation
 docker build . -t kraydel:v1.0.0-rc1
+
+# Run container
+docker run -d -v conf-lbl:/etc/nginx --name nginx-lbl kraydel:v1.0.0-rc1
