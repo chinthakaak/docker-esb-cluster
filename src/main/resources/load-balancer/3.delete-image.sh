a99befd9a6ac4fd0a4bb8a74bb5a0918d0b624fa
kushan <chinthakaak@gmail.com>
2018-01-04 09:28:43 +0530

    addd

--- src/main/resources/load-balancer/3.delete-image.sh
+++ src/main/resources/load-balancer/3.delete-image.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker rmi kraydel:v1.0.0-rc1
+docker rmi nginx-lbl:v1.0.0-rc1
--- src/main/resources/wso2-esb-manager/5.create-image.sh
+++ src/main/resources/wso2-esb-manager/5.create-image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Docker image creation
-docker build . -t nginx-lbl:v1.0.0-rc1
+docker build . -t wso2-esb-manager:v1.0.0-rc1
