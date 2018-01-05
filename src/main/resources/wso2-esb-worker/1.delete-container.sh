3fe9fffbf48b64b3196032ed1a7e85ac42136608
kushan <chinthakaak@gmail.com>
2018-01-05 08:43:52 +0530

    addd

---
+++ src/main/resources/wso2-esb-worker/1.delete-container.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker stop wso2-esb-worker
+docker rm wso2-esb-worker
---
+++ src/main/resources/wso2-esb-worker/5.create-image.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Docker image creation
+docker build . -t wso2-esb-worker:v1.0.0-rc1
