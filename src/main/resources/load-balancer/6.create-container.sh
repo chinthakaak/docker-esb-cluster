28ce027e65cf03f66d8e7f8aafa370698bb78783
kushan <chinthakaak@gmail.com>
2018-01-14 08:26:52 +0530

    addd

--- src/main/resources/load-balancer/6.create-container.sh
+++ src/main/resources/load-balancer/6.create-container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Run container
-docker run -d -v conf-lbl:/etc/nginx -p 80:80 --name nginx-lbl --net net1 --ip 172.18.5.1 nginx-lbl:v1.0.0-rc1
+docker run -d -v conf-lbl:/etc/nginx -h nginx-lbl -p 80:80 -p 443:443 --name nginx-lbl --net net1 --ip 172.18.5.1 nginx-lbl:v1.0.0-rc1
 #docker run --net net1 --ip 172.18.5.1 -it ubuntu
