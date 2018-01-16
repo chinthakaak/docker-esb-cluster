c4334f7a2e05c8309cb0e8f3842bc3d83a11d95c
kushan <chinthakaak@gmail.com>
2018-01-16 23:25:42 +0530

    addd

--- src/main/resources/common/dns/1.create-dns-container.sh
+++ src/main/resources/common/dns/1.create-dns-container.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --name kraydel-dns -d  -p 5380:8080 -p 53:53 -v /opt/dnsmasq.conf:/etc/dnsmasq.conf --net net1 --ip 172.18.5.10 --log-opt "max-size=100m" -e "HTTP_USER=foo"  -e "HTTP_PASS=bar" --restart always  jpillora/dnsmasq
+docker run --name kraydel-dns -d  -p 5380:8080 -p 192.168.1.3:53:53/udp -v /opt/dnsmasq.conf:/etc/dnsmasq.conf --net net1 --ip 172.18.5.10 --log-opt "max-size=100m" -e "HTTP_USER=foo"  -e "HTTP_PASS=bar" --restart always  jpillora/dnsmasq
--- src/main/resources/common/dns/readme.md
+++ src/main/resources/common/dns/readme.md
@@ -3,3 +3,4 @@ Access web ui from http://localhost:5380/
 Add following to /etc/resolv.config
   nameserver 172.18.5.10
 
+https://hub.docker.com/r/jpillora/dnsmasq/
